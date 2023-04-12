0c888cad17dbc03f19b3f36e6a944db7cf4fc6ec
Unmesh93 <129290889+Unmesh93@users.noreply.github.com>
2023-04-12 12:38:06 +0530

    My commit message

---
+++ terraform/snowflake/dgtlweb_raw/psa_profitero/testing2.tf
@@ -0,0 +1,38 @@
+resource "snowflake_table" "BRANDS" {
+    database = local.database
+    schema   = "PSA_PROFITERO"
+     name     = "BRANDS" 
+    comment  = ""
+
+  column {
+    name = "SR." 
+    type = "NUMBER(38,0)"
+    nullable = true 
+}
+  column {
+    name = "NAME" 
+    type = "VARCHAR(16708216)"
+    nullable = true 
+}
+  column {
+    name = "GENDER" 
+    type = "VARCHAR(16708216)"
+    nullable = true 
+}
+  column {
+    name = "AGE" 
+    type = "NUMBER(38,0)"
+    nullable = true 
+}
+  column {
+    name = "DATE " 
+    type = "VARCHAR(16708216)"
+    nullable = true 
+}
+  column {
+    name = "COUNTRY" 
+    type = "VARCHAR(16708216)"
+    nullable = true 
+}
+
+ }
